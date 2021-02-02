65c8dc2a14dc989e152ce2ae123aafec43a05418
Nick <shiroaimai@gmail.com>
2021-02-02 20:03:26 +0100

    Add Match and tilesprite dto

---
+++ Assets/Scripts/Models/Match.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Models
+{
+    [Serializable]
+    public struct Match
+    {
+        public static int MatchMinLength = 2;
+            
+        public Vector2 dir;
+        public List<GameObject> matches;
+    }
+}
---
+++ Assets/Scripts/Models/TileSprite.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace Models
+{
+    [Serializable]
+    public struct TileSprite
+    {
+        public Tile.TileType type;
+        public Sprite sprite;
+    }
+}
